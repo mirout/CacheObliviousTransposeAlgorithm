5a0c1172017c7f6626937c5f97fb28d9110977f2
Mikhail  Ruzavin <misharuz24@gmail.com>
2020-12-28 22:54:44 +0300

    Change project structure

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+pub mod matrix {
+    use std::fmt::{Display, Formatter};
+    use std::fmt;
+    use rand::RngCore;
+
+    pub struct Matrix {
+        n : usize,
+        m : usize,
+        matrix : Vec<Vec<i32>>,
+    }
+
+    impl Matrix {
+        pub fn new(n : usize, m : usize) -> Matrix {
+            Matrix {
+                n,
+                m,
+                matrix: vec![vec![0; m]; n]
+            }
+        }
+
+        pub fn new_random(n : usize, m : usize) -> Matrix {
+            let mut matrix = vec![vec![0; m]; n];
+            let mut rng = rand::thread_rng();
+            for i in 0..n {
+                for j in 0..m {
+                    matrix[i][j] = rng.next_u32() as i32;
+                }
+            }
+
+            Matrix {
+                n,
+                m,
+                matrix,
+            }
+        }
+
+        pub fn transpose(&self) -> Matrix {
+            let mut result = Matrix::new(self.m, self.n);
+            for i in 0..self.n {
+                for j in 0..self.m {
+                    result.matrix[j][i] = self.matrix[i][j];
+                }
+            }
+            result
+        }
+
+        pub fn fast_transpose(&self) -> Matrix {
+            let mut result: Matrix = Matrix::new(self.m, self.n);
+
+            self.fast_transpose_with_recursion(0, 0, self.n, self.m, &mut result);
+
+            result
+        }
+
+        fn fast_transpose_with_recursion(&self, x: usize, y: usize, dx: usize, dy: usize, out: &mut Matrix) {
+            /*
+            x, y - submatrix start
+            dx, dy - submatrix end
+            out - transposed matrix (size: MxN)
+             */
+            if dx == 1 && dy == 1 {
+                out.matrix[y][x] = self.matrix[x][y];
+            } else if dx >= dy {
+                let mid = dx / 2;
+                self.fast_transpose_with_recursion(x, y, mid, dy, out);
+                self.fast_transpose_with_recursion(x+mid, y, dx - mid, dy, out);
+            } else {
+                let mid = dy / 2;
+                self.fast_transpose_with_recursion(x, y, dx, mid, out);
+                self.fast_transpose_with_recursion(x, y + mid, dx, dy - mid, out);
+            }
+        }
+    }
+
+    impl Display for Matrix {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "{:?}", self.matrix)
+        }
+    }
+
+    impl std::cmp::PartialEq for Matrix {
+        fn eq(&self, other: &Self) -> bool {
+            self.matrix == other.matrix
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,37 @@
-use std::fmt::{Display, Formatter};
-use std::fmt;
-use rand::RngCore;
 use std::time::SystemTime;
 
-struct Matrix {
-    n : usize,
-    m : usize,
-    matrix : Vec<Vec<i32>>,
-}
-
-impl Matrix {
-    fn new(n : usize, m : usize) -> Matrix {
-        Matrix {
-            n,
-            m,
-            matrix: vec![vec![0; m]; n]
-        }
-    }
-
-    fn new_random(n : usize, m : usize) -> Matrix {
-        let mut matrix = vec![vec![0; m]; n];
-        let mut rng = rand::thread_rng();
-        for i in 0..n {
-            for j in 0..m {
-                matrix[i][j] = rng.next_u32() as i32;
-            }
-        }
-
-        Matrix {
-            n,
-            m,
-            matrix,
-        }
-    }
-
-    fn transpose(&self) -> Matrix {
-        let mut result = Matrix::new(self.m, self.n);
-        for i in 0..self.n {
-            for j in 0..self.m {
-                result.matrix[j][i] = self.matrix[i][j];
-            }
-        }
-        result
-    }
-
-    fn fast_transpose(&self) -> Matrix {
-        let mut result: Matrix = Matrix::new(self.m, self.n);
-
-        self.fast_transpose_with_recursion(0, 0, self.n, self.m, &mut result);
-
-        result
-    }
-
-    fn fast_transpose_with_recursion(&self, x: usize, y: usize, dx: usize, dy: usize, out: &mut Matrix) {
-        if dx == 1 && dy == 1 {
-            out.matrix[y][x] = self.matrix[x][y];
-        } else if dx >= dy {
-            let mid = dx / 2;
-            self.fast_transpose_with_recursion(x, y, mid, dy, out);
-            self.fast_transpose_with_recursion(x+mid, y, dx - mid, dy, out);
-        } else {
-            let mid = dy / 2;
-            self.fast_transpose_with_recursion(x, y, dx, mid, out);
-            self.fast_transpose_with_recursion(x, y + mid, dx, dy - mid, out);
-        }
-    }
-}
-
-impl Display for Matrix {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self.matrix)
-    }
-}
-
-impl std::cmp::PartialEq for Matrix {
-    fn eq(&self, other: &Self) -> bool {
-        self.matrix == other.matrix
-    }
-}
+use cache_oblivious::matrix::Matrix;
 
 fn main() {
-    for n in (1000..35001).step_by(1000) {
+    for _k in 0..4 {
 
-        println!("Matrix size {0}x{0}", n);
+        let mut result_naive = Vec::new();
+        let mut result_fast = Vec::new();
 
-        let a = Matrix::new_random(n, n);
+        for n in (1000..35001).step_by(1000) {
+            println!("Matrix size {0}x{0}", n);
 
-        let start = SystemTime::now();
-        a.transpose();
-        println!("Naive time: {}ms", SystemTime::now().duration_since(start).unwrap().as_millis());
+            let a = Matrix::new_random(n, 5000);
 
-        let start = SystemTime::now();
-        a.fast_transpose();
-        println!("Fast time: {}ms", SystemTime::now().duration_since(start).unwrap().as_millis());
+            let start = SystemTime::now();
+            a.transpose();
+            result_naive.push(SystemTime::now().duration_since(start).unwrap().as_millis());
+
+            let start = SystemTime::now();
+            a.fast_transpose();
+            result_fast.push(SystemTime::now().duration_since(start).unwrap().as_millis());
+        }
+
+        for i in result_naive {
+            println!("{}", i);
+        }
+
+        println!("----------------");
+
+        for i in result_fast {
+            println!("{}", i);
+        }
+
+        println!("!---------------!");
     }
 }
